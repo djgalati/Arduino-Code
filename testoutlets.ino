eb2d693cf21c62adbc1d948c3001fd819a81125a
DOMENICO GALATI <38479396+djgalati@users.noreply.github.com>
2019-02-28 16:41:15 -0700

    Add files via upload

---
+++ testoutlets.ino
@@ -0,0 +1,27 @@
+/*
+ *  This sketch demonstrates how to set up a simple HTTP-like server.
+ *  The server will set a GPIO pin depending on the request
+ *    http://server_ip/gpio/0 will set the GPIO2 low,
+ *    http://server_ip/gpio/1 will set the GPIO2 high
+ *  server_ip is the IP address of the ESP8266 module, will be 
+ *  printed to Serial when the module is connected.
+ */
+int R1=1;
+void setup() {
+  // prepare GPIO2
+  pinMode(D2, OUTPUT);
+  digitalWrite(D2, R1);     //EVERY RELAY WILL TURN OFF IN THE BEGGINING
+}
+int i=0;
+void loop() {
+  if(i>=1000){
+    digitalWrite(D2, R1);
+    R1=!R1;
+    i=0;
+    
+  }
+  i++;
+  delay(1);
+}
+  
+
