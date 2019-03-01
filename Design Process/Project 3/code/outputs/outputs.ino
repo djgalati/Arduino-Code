9a1fcfc4bb8ee0fcd0880ec4f9ee8f44da5bbede
DOMENICO GALATI <38479396+djgalati@users.noreply.github.com>
2019-03-01 12:06:16 -0700

    Add files via upload

---
+++ Design Process/Project 3/code/outputs/outputs.ino
@@ -0,0 +1,79 @@
+#define pminput  0
+#define pmtrue 1//all defines preset pin number and name for all LED's
+#define M6  2//LSB for Minutes
+#define M5  3
+#define M4  4
+#define M3  5
+#define M2  6
+#define M1  7//MSB for Minutes
+#define H4  8//LSB for Hours
+#define H3  9
+#define H2  10
+#define H1  11//MSB for Hours
+
+int h1=0;//This and the next 10 lines are for each of the LED's for if it's on or off
+int h2=0;
+int h3=0;
+int h4=0;
+int m1=0;
+int m2=0;
+int m3=0;
+int m4=0;
+int m5=0;
+int m6=0;
+bool PMTrue=false;
+int minutes=0;//the decimal number of the currently shown minutes
+int hours=0;//the decimal number of the currently shown hours
+
+
+void setup() {
+  pinMode(A1, INPUT);//This line and the next 2 lines set up the input of what time is being stored on the other arduino
+  pinMode(A2, INPUT);
+  pinMode(pminput, INPUT);
+  pinMode(PMTrue, OUTPUT);//This line and the next 10 lines set the pins as outputs to power the LED's indiviually
+  pinMode(H1, OUTPUT);
+  pinMode(H2, OUTPUT);
+  pinMode(H3, OUTPUT);
+  pinMode(H4, OUTPUT);
+  pinMode(M1, OUTPUT);
+  pinMode(M2, OUTPUT);
+  pinMode(M3, OUTPUT);
+  pinMode(M4, OUTPUT);
+  pinMode(M5, OUTPUT);
+  pinMode(M6, OUTPUT);
+}
+
+void loop() {
+  hours=analogRead(A1);// this line and the next 2 lines will update the time stores on the other arduino everytime the code loops
+  minutes=analogRead(A2);
+  PMTrue=digitalRead(pminput);
+  h1=hours%2;//This line and the next 3 lines take hours stored and 
+  hours=hours/2;
+  h2=h1%2;
+  hours=hours/2;
+  h3=h2%2;
+  hours=hours/2;
+  h4=h3%2;
+  m1=minutes%2;
+  minutes=minutes/2;
+  m2=m1%2;
+  minutes=minutes/2;
+  m3=m2%2;
+  minutes=minutes/2;
+  m4=m3%2;
+  minutes=minutes/2;
+  m5=m4%2;
+  minutes=minutes/2;
+  m6=m5%2;
+  digitalWrite(H1, h1);
+  digitalWrite(H2, h2);
+  digitalWrite(H3, h3);
+  digitalWrite(H4, h4);
+  digitalWrite(M1, m1);
+  digitalWrite(M2, m2);
+  digitalWrite(M3, m3);
+  digitalWrite(M4, m4);
+  digitalWrite(M5, m5);
+  digitalWrite(M6, m6);
+  digitalWrite(pmtrue,PMTrue);
+}
---
+++ Design Process/Project 3/inputs/inputs.ino
@@ -0,0 +1,97 @@
+#include <Servo.h>
+#define IR A0
+#define sw1 0
+#define sw2 1
+#define sw3 2
+#define pmoutput  4
+int seconds=0;
+int minutes=0;
+int hours=0;
+bool PMTrue=false;
+static int steps=60;//tells motor it should take 60 steps to complete 1 revolution
+Servo SecHand;
+
+void setup() {
+pinMode(IR, INPUT);
+pinMode(sw1, INPUT);
+pinMode(sw2, INPUT);
+pinMode(sw3, INPUT);
+pinMode(pmoutput, OUTPUT);
+pinMode(A1, OUTPUT);
+pinMode(A2, OUTPUT);
+pinMode(11, OUTPUT);
+SecHand.attach(11);
+}
+
+void loop() {
+  int choice=0; //setup knob, variable must be set up here for switch(case) statement
+  int Ctemp1=digitalRead(sw2);
+  int Ctemp2=digitalRead(sw3);
+  choice = Ctemp2+(2*Ctemp1);// make sure these are in the proper order
+  switch (choice){
+   case 0:
+   SecHand.write(95);
+     if((analogRead(A0))<255){//must correct this so when the arm passes the IR
+       if(seconds>=59){
+         if(minutes>=59){
+           if(hours>=12){
+             minutes=0;
+             hours=1;
+             PMTrue=!PMTrue;
+           }
+           else if(hours<12) {
+             minutes=0;
+             hours=hours+1;
+           }
+          else if(minutes<59){
+           minutes=minutes+1;
+          }
+        else if(seconds<59){
+          seconds=seconds+1;
+        }
+      else{}
+      }
+    else{}
+    }
+   else{}
+   }
+   case 1:
+   SecHand.write(90);
+    if((digitalRead(sw1))==1){
+      do{
+      }while((digitalRead(sw1))==1);
+      PMTrue=!PMTrue;
+    }
+   case 2:
+   SecHand.write(90);
+    if((digitalRead(sw1))==1){
+     do{
+      }while((digitalRead(sw1))==1);
+      if(minutes>=59){
+        minutes=0;
+      }
+      else{
+        minutes=minutes+1;
+      }
+    }
+   case 3:
+   SecHand.write(90);
+    if((digitalRead(sw1))==1){
+      do{
+      }while((digitalRead(sw1))==1);
+      if(hours>=12){
+        hours=1;
+      }
+      else{
+        hours=hours+1;
+      }
+    }
+  }
+  analogWrite(A1, hours);
+  analogWrite(A2, minutes);
+  digitalWrite(pmoutput, PMTrue);
+}
+
+
+
+
